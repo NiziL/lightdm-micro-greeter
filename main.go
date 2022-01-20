afb208202749f7278d2ed9733f6b7ae8db71595b
NiziL <biasutto.t@gmail.com>
2022-01-20 19:02:22 +0100

    :rocket: basic autologin greeter with hardcoded username/pwd

--- main.go
+++ main.go
@@ -1,17 +1,16 @@
 package main
 
-// We might use this kind of pkg-config later to ensure portability
-//#cgo pkg-config: lightdm-gobject-1
-// But for now, let's stick on the old and ugly way
+// TODO use pkg-config instead of flags to ensure portability
+//#cgo pkg-config: lightdm-gobject-1 (?)
 
 /*
 #cgo CFLAGS: -I/usr/include/lightdm-gobject-1 -I/usr/include/glib-2.0 -I/usr/lib/glib-2.0/include
-#cgo LDFLAGS: -llightdm-gobject-1 -lgobject-2.0
+#cgo LDFLAGS: -llightdm-gobject-1 -lgobject-2.0 -lglib-2.0
 
 #include "lightdm.h"
 
-extern void authentication_complete_cb(void);
-extern void show_prompt_cb(void);
+extern void authentication_complete_cb(LightDMGreeter *greeter);
+extern void show_prompt_cb(LightDMGreeter *greeter, char *text, LightDMPromptType type);
 
 __attribute__((weak)) // TODO that's ugly and I should fix it
 void greeter_signal_connect(LightDMGreeter* greeter) {
@@ -23,37 +22,58 @@ import "C"
 
 import (
     "unsafe"
+    "fmt"
 	"log"
 )
 
 //export authentication_complete_cb
-func authentication_complete_cb() {
-	log.Print("authentication cb called!")
+func authentication_complete_cb(greeter *C.LightDMGreeter) {
+    log.Print("authentication_complete_cb called!")
+
+    if C.lightdm_greeter_get_is_authenticated(greeter) == 0 {
+        log.Print("wrong password dumbass")
+    } else {
+        C.lightdm_greeter_start_session_sync(greeter, nil, nil)
+    }
 }
 
 //export show_prompt_cb
-func show_prompt_cb() {
-    log.Print("prompt cb called!")
+func show_prompt_cb(greeter *C.LightDMGreeter, text *C.char, prompt_type C.LightDMPromptType) {
+    log.Print("show_prompt_cb called!")
+
+    // TODO let user give password 
+    password := "<password>"
+    
+    c_password := C.CString(password)
+    C.lightdm_greeter_respond(greeter, c_password, nil) 
 }
 
 func main() {
-	log.Print("lightdm-micro-greeter start up")
+    log.Print("lightdm-micro-greeter start up")
 
-	greeter := C.lightdm_greeter_new()
-    //defer C.free(unsafe.Pointer(greeter)) // TODO invalid pointer ?
-	log.Print("greeter created")
+    loop := C.g_main_loop_new(nil, 0)
+    // should i defer free loop ?
 
-	if C.lightdm_greeter_connect_to_daemon_sync(greeter, nil) > 0 {
-		log.Fatal("Not sync to daemon !")
-	}
-	log.Print("greeter connected to lightdm deamon")
+    greeter := C.lightdm_greeter_new()
+    //defer C.free(unsafe.Pointer(greeter)) // TODO fix invalid pointer at runtime, should I really free this ? Does glib free it for me ?
+    log.Print("greeter created")
+
+    if C.lightdm_greeter_connect_to_daemon_sync(greeter, nil) == 0 {
+        log.Fatal("Not sync to daemon !")
+    }
+    log.Print("greeter connected to lightdm deamon")
 
     C.greeter_signal_connect(greeter)
+    log.Print("greeter callbacks binded")
 
-    username := C.CString("user")
-    defer C.free(unsafe.Pointer(username))
-    C.lightdm_greeter_authenticate(greeter, username, nil)
+    // TODO let user give username
+    username := "<user>"
 
-    for true {
-    }
+    c_username := C.CString(username)
+    defer C.free(unsafe.Pointer(c_username))
+
+    C.lightdm_greeter_authenticate(greeter, c_username, nil)
+    log.Print("authentication started")
+    
+    C.g_main_loop_run(loop)
 }
