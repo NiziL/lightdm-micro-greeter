223bde76263aa5558f4cfadbf239bbd34e0e9f38
NiziL <biasutto.t@gmail.com>
2022-01-26 16:13:18 +0100

    first experiment around gtk: working but terrible ux (enter on username entry then enter on pwd), lightdm show_prompt_cb never used

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/nizil/lightdm-micro-greeter
 
 go 1.17
 
-require github.com/gotk3/gotk3 v0.6.1 // indirect
+require github.com/gotk3/gotk3 v0.6.1
--- main.go
+++ main.go
@@ -12,68 +12,103 @@ package main
 extern void authentication_complete_cb(LightDMGreeter *greeter);
 extern void show_prompt_cb(LightDMGreeter *greeter, char *text, LightDMPromptType type);
 
-__attribute__((weak)) // TODO that's ugly and I should fix it
+// TODO that's ugly and I should fix it
+// preamble should not contain any definition when using //export later
+__attribute__((weak))
 void greeter_signal_connect(LightDMGreeter* greeter) {
-    g_signal_connect(greeter, "authentication-complete", G_CALLBACK(authentication_complete_cb), NULL);
-    g_signal_connect(greeter, "show-prompt", G_CALLBACK(show_prompt_cb), NULL);
+	g_signal_connect(greeter, "authentication-complete", G_CALLBACK(authentication_complete_cb), NULL);
+	g_signal_connect(greeter, "show-prompt", G_CALLBACK(show_prompt_cb), NULL);
 }
 */
 import "C"
 
 import (
-    "unsafe"
-    "fmt"
 	"log"
+	"unsafe"
+
+	"github.com/gotk3/gotk3/gtk"
 )
 
 //export authentication_complete_cb
 func authentication_complete_cb(greeter *C.LightDMGreeter) {
-    log.Print("authentication_complete_cb called!")
+	log.Print("authentication_complete_cb called!")
 
-    if C.lightdm_greeter_get_is_authenticated(greeter) == 0 {
-        log.Print("wrong password dumbass")
-    } else {
-        C.lightdm_greeter_start_session_sync(greeter, nil, nil)
-    }
+	if C.lightdm_greeter_get_is_authenticated(greeter) == 0 {
+		log.Print("wrong password dumbass")
+	} else {
+		C.lightdm_greeter_start_session_sync(greeter, nil, nil)
+	}
 }
 
 //export show_prompt_cb
 func show_prompt_cb(greeter *C.LightDMGreeter, text *C.char, prompt_type C.LightDMPromptType) {
-    log.Print("show_prompt_cb called!")
+	log.Print("show_prompt_cb called!")
 
-    // TODO let user give password 
-    password := "<password>"
-    
-    c_password := C.CString(password)
-    C.lightdm_greeter_respond(greeter, c_password, nil) 
+	// TODO let user give password
+	//c_password := C.CString(password)
+	//defer C.free(unsafe.Pointer(c_password))
+	//C.lightdm_greeter_respond(greeter, c_password, nil)
 }
 
 func main() {
-    log.Print("lightdm-micro-greeter start up")
+	log.Print("lightdm-micro-greeter start up")
 
-    loop := C.g_main_loop_new(nil, 0)
-    // should i defer free loop ?
+	greeter := C.lightdm_greeter_new()
+	// TODO fix invalid pointer at runtime
+	// defer C.free(unsafe.Pointer(greeter))
+	// should I really free this ? Does glib free it for me ? should I use g_free ?
+	log.Print("greeter created")
 
-    greeter := C.lightdm_greeter_new()
-    //defer C.free(unsafe.Pointer(greeter)) // TODO fix invalid pointer at runtime, should I really free this ? Does glib free it for me ?
-    log.Print("greeter created")
+	if C.lightdm_greeter_connect_to_daemon_sync(greeter, nil) == 0 {
+		log.Fatal("Not sync to daemon !")
+	}
+	log.Print("greeter connected to lightdm deamon")
 
-    if C.lightdm_greeter_connect_to_daemon_sync(greeter, nil) == 0 {
-        log.Fatal("Not sync to daemon !")
-    }
-    log.Print("greeter connected to lightdm deamon")
+	C.greeter_signal_connect(greeter)
+	log.Print("greeter callbacks binded")
 
-    C.greeter_signal_connect(greeter)
-    log.Print("greeter callbacks binded")
+	gtk.Init(nil)
+	log.Print("gtk init")
 
-    // TODO let user give username
-    username := "<user>"
+	win, err := gtk.WindowNew(gtk.WINDOW_TOPLEVEL)
+	if err != nil {
+		log.Fatal("unable to create window:", err)
+	}
+	win.Connect("destroy", func() {
+		gtk.MainQuit()
+	})
 
-    c_username := C.CString(username)
-    defer C.free(unsafe.Pointer(c_username))
+    grid, _ := gtk.GridNew()
+    grid.SetOrientation(gtk.ORIENTATION_VERTICAL)
 
-    C.lightdm_greeter_authenticate(greeter, c_username, nil)
-    log.Print("authentication started")
+	l, _ := gtk.LabelNew("username")
+	grid.Add(l)
+
+	entry, _ := gtk.EntryNew()
+	entry.Connect("activate", func() {
+		username, _ := entry.GetText()
+		c_username := C.CString(username)
+		defer C.free(unsafe.Pointer(c_username))
+		log.Print("starting authentication for ", username)
+		C.lightdm_greeter_authenticate(greeter, c_username, nil)
+	})
+    grid.AttachNextTo(entry, l, gtk.POS_RIGHT, 1, 1)
+
+	l_pwd, _ := gtk.LabelNew("password")
+	grid.Add(l_pwd)
+
+	pwd_entry, _ := gtk.EntryNew()
+    pwd_entry.SetVisibility(false)
+	pwd_entry.Connect("activate", func() {
+		pwd, _ := pwd_entry.GetText()
+		c_pwd := C.CString(pwd)
+		defer C.free(unsafe.Pointer(c_pwd))
+        log.Print("sending password")
+		C.lightdm_greeter_respond(greeter, c_pwd, nil)
+	})
+    grid.AttachNextTo(pwd_entry, l_pwd, gtk.POS_RIGHT, 1, 1)
     
-    C.g_main_loop_run(loop)
+    win.Add(grid)
+	win.ShowAll()
+	gtk.Main()
 }
