48acdff13296d121be7aa4376145dd3c02757bb5
NiziL <biasutto.t@gmail.com>
2022-01-27 19:22:53 +0100

    I can't understand how cgo is working

--- greeter_signal_connect.c
+++ greeter_signal_connect.c
@@ -1,9 +1,6 @@
 #define connect(name, func) g_signal_connect(greeter, name, G_CALLBACK(func), NULL)
 #include "_cgo_export.h"
 
-extern void authentication_complete_cb(LightDMGreeter *greeter);
-extern void show_prompt_cb(LightDMGreeter *greeter, char *text, LightDMPromptType type);
-
 void greeter_signal_connect(LightDMGreeter* greeter) {
     connect("authentication-complete", authentication_complete_cb);
     connect("show-prompt", show_prompt_cb);
