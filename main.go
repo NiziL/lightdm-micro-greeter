750e9fe723bca05881168a77021e3539b3d23612
NiziL <biasutto.t@gmail.com>
2022-01-27 16:13:40 +0100

    better handling of gtk and lightdm callback, slightly better UX

--- main.go
+++ main.go
@@ -29,27 +29,62 @@ import (
 	"github.com/gotk3/gotk3/gtk"
 )
 
+// TODO I'd like to avoid this global var
+var waiting_pwd, waiting_resp bool
+var entry *gtk.Entry
+var label *gtk.Label
+
 //export authentication_complete_cb
 func authentication_complete_cb(greeter *C.LightDMGreeter) {
 	log.Print("authentication_complete_cb called!")
 
 	if C.lightdm_greeter_get_is_authenticated(greeter) == 0 {
-		log.Print("wrong password dumbass")
+		log.Print("wrong password")
+		waiting_pwd = false
 	} else {
 		C.lightdm_greeter_start_session_sync(greeter, nil, nil)
 	}
+	waiting_resp = false
 }
 
 //export show_prompt_cb
 func show_prompt_cb(greeter *C.LightDMGreeter, text *C.char, prompt_type C.LightDMPromptType) {
+	// text is the lightdm deamon answer, have seen nothing else than "password:"
 	log.Print("show_prompt_cb called!")
-	//go_text := C.GoString(text)
-	// go_text is the lightdm deamon answer, can be "password:"	or wrong login
-	// TODO let user give password
+	waiting_pwd = true
+}
+
+// may I use this builder to avoid global var (at least entry and label) ?
+func create_entry_cb(greeter *C.LightDMGreeter) func() {
+	return func() {
+		input, _ := entry.GetText()
+		entry.SetText("")
+
+		c_input := C.CString(input)
+		defer C.free(unsafe.Pointer(c_input))
+
+		if waiting_pwd {
+			log.Print("pwd entered")
+			C.lightdm_greeter_respond(greeter, c_input, nil)
+			waiting_resp = true
+			label.SetText("username: ")
+			entry.SetVisibility(true)
+		} else if !waiting_resp {
+			log.Print("starting authentication for ", input)
+			C.lightdm_greeter_authenticate(greeter, c_input, nil)
+			label.SetText("password: ")
+			entry.SetVisibility(false)
+		}
+	}
 }
 
 func main() {
 	log.Print("lightdm-micro-greeter start up")
+
+	var err error
+
+	waiting_pwd = false
+	waiting_resp = false
 
 	greeter := C.lightdm_greeter_new()
 	// TODO fix invalid pointer at runtime
@@ -75,42 +110,21 @@ func main() {
 	win.Connect("destroy", func() {
 		gtk.MainQuit()
 	})
-	display, _ := win.GetDisplay()
-	monitor, _ := display.GetPrimaryMonitor()
+	display, err := win.GetDisplay()
+	monitor, err := display.GetPrimaryMonitor()
 	rect := monitor.GetGeometry()
 	win.Resize(rect.GetWidth(), rect.GetHeight())
 
-	grid, _ := gtk.GridNew()
+	grid, err := gtk.GridNew()
 	grid.SetOrientation(gtk.ORIENTATION_VERTICAL)
 
-	l, _ := gtk.LabelNew("username")
-	grid.Add(l)
+	label, err = gtk.LabelNew("username:")
+	grid.Add(label)
 
-	entry, _ := gtk.EntryNew()
-	grid.AttachNextTo(entry, l, gtk.POS_RIGHT, 1, 1)
+	entry, err = gtk.EntryNew()
+	grid.AttachNextTo(entry, label, gtk.POS_RIGHT, 1, 1)
 
-	l_pwd, _ := gtk.LabelNew("password")
-	grid.Add(l_pwd)
-
-	pwd_entry, _ := gtk.EntryNew()
-	pwd_entry.SetVisibility(false)
-	grid.AttachNextTo(pwd_entry, l_pwd, gtk.POS_RIGHT, 1, 1)
-
-	entry.Connect("activate", func() {
-		username, _ := entry.GetText()
-		c_username := C.CString(username)
-		defer C.free(unsafe.Pointer(c_username))
-		log.Print("starting authentication for ", username)
-		C.lightdm_greeter_authenticate(greeter, c_username, nil)
-	})
-
-	pwd_entry.Connect("activate", func() {
-		pwd, _ := pwd_entry.GetText()
-		c_pwd := C.CString(pwd)
-		defer C.free(unsafe.Pointer(c_pwd))
-		log.Print("sending password")
-		C.lightdm_greeter_respond(greeter, c_pwd, nil)
-	})
+	entry.Connect("activate", create_entry_cb(greeter))
 
 	win.Add(grid)
 	win.ShowAll()
