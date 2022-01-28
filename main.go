4f05ec3ec2deaef6b2ef7c6edb60530c035e672e
NiziL <biasutto.t@gmail.com>
2022-01-28 11:22:45 +0100

    useless flags removed

--- main.go
+++ main.go
@@ -17,8 +17,7 @@ import (
 	"github.com/gotk3/gotk3/gtk"
 )
 
-// TODO I'd like to avoid this global var
-var waiting_pwd, waiting_resp bool
+// TODO I'd like to avoid these global vars
 var entry *gtk.Entry
 var label *gtk.Label
 
@@ -28,18 +27,22 @@ func authentication_complete_cb(greeter *C.LightDMGreeter) {
 
 	if C.lightdm_greeter_get_is_authenticated(greeter) == 0 {
 		log.Print("wrong password")
-		waiting_pwd = false
 	} else {
 		C.lightdm_greeter_start_session_sync(greeter, nil, nil)
 	}
-	waiting_resp = false
 }
 
 //export show_prompt_cb
 func show_prompt_cb(greeter *C.LightDMGreeter, text *C.char, prompt_type C.LightDMPromptType) {
 	// text is the lightdm deamon answer, have seen nothing else than "password:"
 	log.Print("show_prompt_cb called!")
-	waiting_pwd = true
+	txt := C.GoString(text)
+	label.SetText(txt)
+	if prompt_type == C.LIGHTDM_PROMPT_TYPE_SECRET {
+		entry.SetVisibility(false)
+	} else {
+		entry.SetVisibility(true)
+	}
 }
 
 // may I use this builder to avoid global var (at least entry and label) ?
@@ -51,17 +54,15 @@ func create_entry_cb(greeter *C.LightDMGreeter) func() {
 		c_input := C.CString(input)
 		defer C.free(unsafe.Pointer(c_input))
 
-		if waiting_pwd {
-			log.Print("pwd entered")
+		if C.lightdm_greeter_get_is_authenticated(greeter) != 0 {
+			// start_session ?
+			log.Print("authentication ok")
+		} else if C.lightdm_greeter_get_in_authentication(greeter) != 0 {
+			// give pwd
 			C.lightdm_greeter_respond(greeter, c_input, nil)
-			waiting_resp = true
-			label.SetText("username: ")
-			entry.SetVisibility(true)
-		} else if !waiting_resp {
-			log.Print("starting authentication for ", input)
+		} else {
+			// give username
 			C.lightdm_greeter_authenticate(greeter, c_input, nil)
-			label.SetText("password: ")
-			entry.SetVisibility(false)
 		}
 	}
 }
@@ -70,9 +71,6 @@ func main() {
 	log.Print("lightdm-micro-greeter start up")
 
 	var err error
-
-	waiting_pwd = false
-	waiting_resp = false
 
 	greeter := C.lightdm_greeter_new()
 	// TODO fix invalid pointer at runtime
